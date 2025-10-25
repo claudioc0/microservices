cbc9b84e8244c521405b48691e11380ccaa02838
unknown <claudio.colombo@pucpr.edu.br>
2025-10-25 10:37:14 -0300

    Merge: Junção do repositório remoto com projeto local

--- microservice-contas/index.js
+++ microservice-contas/index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 require('dotenv').config();
 
-=======
->>>>>>> e18ea1c4509abc2261c5ecf0b29c13d7960e015e
 const express = require('express');
 const { Connection, Request } = require('tedious');
 
@@ -25,7 +22,6 @@ const config = {
   }
 };
 
-<<<<<<< HEAD
 // Endpoint para buscar conta por ID 
 app.get('/contas/:id', (req, res) => {
   const connection = new Connection(config);
@@ -54,26 +50,6 @@ app.get('/contas/:id', (req, res) => {
     connection.execSql(request);
   });
   connection.connect();
-=======
-// Endpoint para buscar conta por ID
-app.get('/contas/:id', (req, res) => {
-  const connection = new Connection(config);
-  connection.on('connect', (err) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    const request = new Request(`SELECT * FROM Contas WHERE id = '${req.params.id}' FOR JSON PATH, WITHOUT_ARRAY_WRAPPER`, (err, rowCount, rows) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).json(JSON.parse(rows[0][0].value));
-      }
-      connection.close();
-    });
-    connection.execSql(request);
-  });
-  connection.connect();
->>>>>>> e18ea1c4509abc2261c5ecf0b29c13d7960e015e
 });
 
 // Adicione endpoints para CREATE, UPDATE, DELETE...
